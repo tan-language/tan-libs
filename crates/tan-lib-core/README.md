05e4618534a22b5fef237b61b02453588f265103
Georgios Moschovitis <gmosx@reizu.com>
2024-08-16 13:11:57 +0300

    Remove unused core libs. #chore

--- crates/tan-lib-core/README.md
+++ crates/tan-lib-core/README.md
@@ -1,9 +1,12 @@
 # Core Foreign Library
 
+#warning NOT USED yet.
+
 Foreign code liked statically to the tan executable.
 
 ## Todo
 
 - This crate should be statically linked.
-- How should we publish this, how should this be linked with the tan interpreter. #blocker
+- How should we publish this, how should this be linked with the tan
+  interpreter. #blocker
   - not sure we need to publish this create, but still, lets add the tan-prefix
--- crates/tan-lib-core/src/lib.rs
+++ crates/tan-lib-core/src/lib.rs
@@ -1,51 +1,9 @@
-pub mod arithmetic;
-pub mod bool;
-pub mod buffer;
-pub mod char;
-pub mod chrono;
-pub mod cmp;
-pub mod css_expr;
-pub mod dec;
-pub mod eq;
-pub mod error;
-pub mod float;
-pub mod fs;
-pub mod html;
-pub mod int;
-pub mod io;
-pub mod json;
-pub mod lang;
-pub mod map;
-pub mod math;
-pub mod maybe;
-pub mod path;
-pub mod prelude;
-pub mod process;
-pub mod range;
-pub mod regex;
-pub mod rng;
-pub mod seq;
-pub mod set;
-pub mod string;
-pub mod testing;
-pub mod u8;
 pub mod uuid;
 
-use dec::setup_lib_dec;
-
 use tan::context::Context;
+use uuid::setup_lib_uuid;
 
-use self::{
-    chrono::setup_lib_chrono, css_expr::setup_lib_css_expr, fs::setup_lib_fs, html::setup_lib_html,
-    json::setup_lib_codec_json, math::setup_lib_math, path::setup_lib_path,
-    prelude::setup_lib_prelude, process::setup_lib_process, regex::setup_lib_regex,
-    rng::setup_lib_rand, set::setup_lib_set, testing::setup_lib_testing, uuid::setup_lib_uuid,
-};
-
-// #todo consider extracting as a (temporary?) crate, e.g. tan-stdlib-native, tan-native-lib, tan-runtime
-// #todo add unit test for the foreign-functions.
-
-// #todo consider extracting builtins.
+// #todo Move all the code from tan/library.
 
 // #todo helper function or macro for arithmetic operations!
 // #todo also eval 'if', 'do', 'for' and other keywords here!
@@ -72,20 +30,5 @@ use self::{
 
 // #todo find a good name for this.
 pub fn setup_lib(context: &mut Context) {
-    setup_lib_fs(context);
-    setup_lib_process(context);
-    setup_lib_rand(context);
-    setup_lib_html(context);
-    setup_lib_css_expr(context);
-    setup_lib_codec_json(context);
-    setup_lib_math(context);
-    setup_lib_chrono(context);
-    setup_lib_testing(context);
-    setup_lib_path(context);
     setup_lib_uuid(context);
-    setup_lib_regex(context);
-    setup_lib_set(context);
-    setup_lib_dec(context);
-
-    setup_lib_prelude(context);
 }
