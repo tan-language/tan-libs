b933d39a290045808b5c21d8dee8a8895779f686
Georgios Moschovitis <gmosx@reizu.com>
2024-09-25 11:21:12 +0300

    Add random-float. #feat

--- crates/lib-tan-rng/src/rng.rs
+++ crates/lib-tan-rng/src/rng.rs
@@ -2,6 +2,11 @@
 
 // #todo move out of standard library into the common library
 
+// #todo Investigate the design of C++
+// static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+// static std::mt19937 generator;
+// return distribution(generator);
+
 // #todo random_int, random_float
 // #todo should take a range trait.
 // #todo support random-number-generator 'object', with more specialization
@@ -15,7 +20,11 @@
 // (use random)
 // (let n (random/int 5))
 // (let n (random/float 5))
-// (let n (random/num 5)) ; generic<
+// (let n (random/num 5)) ; generic
+//
+// (use [RNG] /random)
+// (let rng (RNG))
+// (let n (gen-float rng 5.0))
 
 // #todo add support for seeding.
 
@@ -23,7 +32,12 @@
 
 use rand::Rng;
 
-use tan::{context::Context, error::Error, expr::Expr, util::module_util::require_module};
+use tan::{
+    context::Context,
+    error::Error,
+    expr::Expr,
+    util::{args::unpack_float_arg, module_util::require_module},
+};
 
 /// (random 100) returns a random integer in the range 0..100
 pub fn random_int(args: &[Expr]) -> Result<Expr, Error> {
@@ -46,6 +60,35 @@ pub fn random_int(args: &[Expr]) -> Result<Expr, Error> {
     }
 }
 
+// #todo Should make random generic, and also work with type-inference.
+// #todo Generics could also reuse the `/` notation, e.g. (random/float 100.0)
+
+/// (random-float) returns a random integer in the range 0.0..1.0
+/// (random-float 100.0) returns a random integer in the range 0.0..100.0
+/// (random-float 20.0 100.0) returns a random integer in the range 20.0..100.0
+pub fn random_float(args: &[Expr]) -> Result<Expr, Error> {
+    // #todo Also support a range argument!
+
+    let mut rng = rand::thread_rng();
+
+    match args.len() {
+        0 => Ok(Expr::Float(rng.gen())),
+        1 => {
+            let end = unpack_float_arg(args, 0, "end")?;
+            Ok(Expr::Float(rng.gen_range(0.0..end)))
+        }
+        2 => {
+            let start = unpack_float_arg(args, 0, "start")?;
+            let end = unpack_float_arg(args, 1, "end")?;
+            Ok(Expr::Float(rng.gen_range(start..end)))
+        }
+        _ => Err(Error::invalid_arguments(
+            "expected at least one argument to random-float",
+            None,
+        )),
+    }
+}
+
 pub fn import_lib_rng(context: &mut Context) {
     // #todo What is a good path? should avoid math?
     // #todo The module path `rand` is better.
@@ -53,6 +96,8 @@ pub fn import_lib_rng(context: &mut Context) {
 
     // #todo better name?
     module.insert("random", Expr::foreign_func(&random_int));
+    // #todo better name?
+    module.insert("random-float", Expr::foreign_func(&random_float));
 }
 
 // #todo add unit tests.
